3b9762a3814457ff77a79b08c455008b3db0aed1
doug_git_hub <skipbox500@gmail.com>
2018-02-18 07:58:46 +0800

    works i think

--- app/src/main/java/com/example/laowai/xxcc/MainActivity.java
+++ app/src/main/java/com/example/laowai/xxcc/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.laowai.xxcc;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+String it_works = "jjjjjjj";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //this is a comment
 }
